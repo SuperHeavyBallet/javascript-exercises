51eb5c06a0d63d305f94001f78c42ae53ec2626c
Alexander Gorham <superheavyballet@gmail.com>
2023-09-23 18:15:53 +0200

    Solved The Tests :>

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+
+    //Run Through Each Element in the given Array, if the Element Matches an Element in the Array of Elements to Remove, Change the element to X
+  
+for (let i = 0; i < array.length; i++)
+{
+
+    for (let j = 0; j < elementsToRemove.length; j++)
+    {
+        if (array[i] === elementsToRemove[j])
+        {
+            array[i] = 'x';
+
+        }
+}
+
+    
+}
+
+// Create a new, empty Array and go through the now fully Marked given array. If an element is NOT marked as X, add it to the end of the new Array
+let endArray = []
+
+for (let i = 0; i < array.length; i++)
+{
+    if (array[i] !== 'x')
+    {
+        endArray.push(array[i]);
+    }
+}
+
+
+return endArray;
 
 };
 
