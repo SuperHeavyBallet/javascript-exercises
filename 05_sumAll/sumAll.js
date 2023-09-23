0aeed9ec9d5ea87fd40f2abcf20d68d8126fa3a9
Alexander Gorham <superheavyballet@gmail.com>
2023-09-23 19:09:33 +0200

    Completed the tests :>

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,51 @@
-const sumAll = function() {
+const sumAll = function(...numbersToAdd) {
+
+    let newNumbersToAdd = [];
+
+    if (numbersToAdd[0] < 0 || numbersToAdd[1] < 0 )
+    {
+        return 'ERROR';
+        console.log("REACHED ERROR VIA NEGATIVE");
+    }
+
+    else if (typeof(numbersToAdd[1]) !== 'number')
+    {
+        return 'ERROR';
+        console.log("REACHED ERROR VIA TYPE");
+    }
+
+
+    else
+    {
+    
+
+        if (numbersToAdd[0] > numbersToAdd[1])
+        {
+          newNumbersToAdd[0] = numbersToAdd[1];
+          newNumbersToAdd[1] = numbersToAdd[0];
+        }
+
+         else if (numbersToAdd[0] <= numbersToAdd[1])
+        {
+            newNumbersToAdd[0] = numbersToAdd[0];
+           newNumbersToAdd[1] = numbersToAdd[1];
+        }
+
+        const firstInteger = newNumbersToAdd[0];
+        const lastInteger =  newNumbersToAdd[newNumbersToAdd.length -1];
+        let runningTotal = 0;
+
+
+        for (let i = 0; i <= lastInteger; i++)
+        {
+            runningTotal = runningTotal + i;
+            //console.log(runningTotal);
+        }
+    
+
+
+        return runningTotal;
+    }
 
 };
 
