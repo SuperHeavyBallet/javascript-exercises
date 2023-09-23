31225311c8ff4d7debc953b8588db3d0fe4839bd
Alexander Gorham <superheavyballet@gmail.com>
2023-09-23 21:31:55 +0200

    Solved the test :>

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+
+  rawTemperature = ((inputFahrenheit - 32) * 5 /9);
+
+  roundedTemperature = Math.round(rawTemperature * 10) / 10;
+
+  return roundedTemperature;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelsius) {
+
+  rawTemperature = (((inputCelsius * 9/5)) + 32);
+  
+  roundedTemperature = Math.round(rawTemperature * 10) / 10;
+
+  return roundedTemperature;
 };
 
 // Do not edit below this line
