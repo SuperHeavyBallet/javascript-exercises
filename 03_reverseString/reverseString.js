74ed1fc4a77fda9e2617eecd294a5800a27d1538
Alexander Gorham <superheavyballet@gmail.com>
2023-09-24 13:06:34 +0200

    Solved the test :>

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,22 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let stringAsArray = inputString.split("");
+    let reversedArray = [];
+
+
+
+    for (let i = stringAsArray.length -1; i > -1; i--)
+    {
+         reversedArray.push(stringAsArray[i]);
+
+    }
+
+    let reverseString = reversedArray.join("");
+
+
+    return(reverseString);
+
+
 
 };
 
