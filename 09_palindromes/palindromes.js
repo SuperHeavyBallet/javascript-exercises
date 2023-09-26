85da40629abd5978717e9c47de6a2789752e0206
Alexander Gorham <agram@hotmail.co.uk>
2023-09-26 20:01:56 +0200

    Completed the tests :>

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,53 @@
-const palindromes = function () {
+const palindromes = function (originalString) {
 
+    stringLower = originalString.toLowerCase();
+
+    let punctuation = (/[\.,?!\s]/g);
+    let stringStripped = originalString.replace(punctuation, "");
+    let stringForwards = stringStripped.toLowerCase();
+
+    
+
+    
+    
+    arrayForwards = stringForwards.split("");
+
+    /*
+    for (let j = 0; j < arrayForwards.length; j++)
+    {
+        if (arrayForwards[j] === '!')
+        {
+            arrayForwards.splice(j,1);
+        }
+    
+    }*/
+
+
+
+
+    let arrayBackwards = [];
+    
+    //console.log(arrayForwards);
+
+    for (let i = 0; i < arrayForwards.length; i++)
+    {
+      arrayBackwards.unshift(arrayForwards[i]);
+
+    }
+
+
+   
+
+    stringForwards = arrayForwards.join("");
+    stringBackwards = arrayBackwards.join("");
+
+    console.log("For: "+ stringForwards);
+    console.log("Back: " + stringBackwards);
+
+    
+
+
+    return stringBackwards === stringForwards;
 };
 
 // Do not edit below this line
