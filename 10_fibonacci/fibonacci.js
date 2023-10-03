66ab4864b9cad6413914dd5e330c792143301a4e
Alexander Gorham <agram@hotmail.co.uk>
2023-10-03 03:20:55 +0200

    Solved the tests, very pleasant experience :>

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(inputNumber) {
+
+    let array = [];
+    array[0] = 0;
+    array[1] = 1;
+
+    if (inputNumber > 0) {
+
+    for (let i = 2; i < inputNumber+1; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+
+    console.log(array);
+    console.log(array[inputNumber]);
+
+    return array[inputNumber];
+}
+
+return "OOPS";
 
 };
 
